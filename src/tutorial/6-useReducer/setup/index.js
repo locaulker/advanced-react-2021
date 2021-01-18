f3db4e49d7bb1f8c7f929f844a92dfd79ed2bdff
Lawrence <locaulker@gmail.com>
2021-01-18 01:42:50 -0500

    Ending at L68 - PropDrilling

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,28 +1,42 @@
 import React, { useState, useReducer } from 'react'
 import Modal from './Modal'
 import { data } from '../../../data'
-// reducer function
 
+// reducer function
 const reducer = (state, action) => {
-  //
+  console.log(state)
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload]
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'item added'
+    }
+  }
+  throw new Error('no matching action type')
 }
 
+// defaultState function
 const defaultState = {
   people: [],
   isModalOpen: false,
-  modalContent: '',
+  modalContent: ''
 }
 
+// Index Component
 const Index = () => {
   const [name, setName] = useState('')
   const [state, dispatch] = useReducer(reducer, defaultState)
 
   const handleSubmit = e => {
     e.preventDefault()
+    const newItem = { id: new Date().getTime().toString(), name }
     if (name) {
-      //
+      dispatch({ type: 'ADD_ITEM', payload: newItem })
+      setName('')
     } else {
-      //
+      dispatch({ type: 'NO_VALUE' })
     }
   }
 
